423e934469723973d9f8aa03567d4a0e01cdc778
MarekOnd <ondrejtablet@gmail.com>
2024-11-24 20:03:10 +0100

    first draft of scenario tiles, added .vscode to gitignore

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
+    import ScenarioTile from "./scenarioTile.svelte";
     let { data } = $props();
 </script>
 
-{#each data.scenarios as scenario}
-    <!-- The script searchParam is only used when it is not the default main.nsl (so just so the url looks nicer) -->
-    <a
-        href="/game/{scenario.dirName}{scenario.metadata.entrypoint !==
-        'main.nls'
-            ? `?script=${scenario.metadata.entrypoint}`
-            : ''}">{scenario.metadata.name}</a
-    >
-    <p>{scenario.metadata.description}</p>
-    <p>Made by: {scenario.metadata.author}</p>
-    <img
-        src="scenarios/{scenario.dirName}/{scenario.metadata.icon}"
-        alt="An icon for the scenario"
-    />
-{/each}
+<div class="mainTitle">Hry</div>
+
+<div class="scenarios">
+    {#each data.scenarios as scenario}
+        <!-- The script searchParam is only used when it is not the default main.nsl (so just so the url looks nicer) -->
+        <ScenarioTile {scenario}></ScenarioTile>
+    {/each}
+</div>
+
+<style>
+    .mainTitle {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 10rem;
+        text-align: center;
+    }
+    .scenarios {
+        width: 80vw;
+        margin-left: 10vw;
+        margin-right: 10vw;
+    }
+</style>
