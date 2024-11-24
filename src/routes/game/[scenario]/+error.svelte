b5dc00c0ba61f386c2dbd789f741c5761ecf6c0f
Pesopes <jeniktelefon@gmail.com>
2024-11-24 14:57:52 +0100

    new fun error 404 page

---
+++ src/routes/game/[scenario]/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { page } from "$app/stores";
+    import Dialogue from "./dialogue.svelte";
+</script>
+
+<div>
+    <Dialogue
+        text={$page.error?.message || "Undefined error"}
+        name={$page.status.toString()}
+    />
+</div>
+
+<style>
+    div {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+        height: 200px;
+        background-color: rebeccapurple;
+    }
+</style>
--- src/routes/game/[scenario]/+page.ts
+++ src/routes/game/[scenario]/+page.ts
@@ -1,8 +1,12 @@
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { parseScript } from './parser';
 
 export const load: PageLoad = async ({ params, fetch }) => {
 	const res = await fetch(`/scenarios/${params.scenario}/main.nls`);
+	if (!res.ok) {
+		error(404, `The scenario "${params.scenario}" couldn't be found`)
+	}
 	const text = await res.text();
 
 	return {
