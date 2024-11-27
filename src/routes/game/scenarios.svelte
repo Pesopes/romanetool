f51c5a145adfa8102244c52178fbde81d9f0c775
Pesopes <jeniktelefon@gmail.com>
2024-11-27 18:46:41 +0100

    scenario picker folder styling done

--- src/routes/game/scenarios.svelte
+++ src/routes/game/scenarios.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import type { Scenario } from "./proxy+page.server";
     let { scenarios }: { scenarios: Scenario[] } = $props();
-    let activeCard = $state(0); // TODO
+    let scrollY = $state(0);
 </script>
 
+<svelte:window bind:scrollY />
 <div class="card-container">
     {#each scenarios as scenario, i}
         <a
@@ -11,8 +12,8 @@
             'main.nls'
                 ? `?script=${scenario.metadata.entrypoint}`
                 : ''}"
-            class="card {activeCard == i ? 'active-card' : 'inactive-card'}"
-            style="top: {i * 350}px;"
+            class="card"
+            style="transform: translateX({10 * i - scrollY / 50}px)"
         >
             <!-- <div class="file-top-main">hello</div> -->
             <!-- The script searchParam is only used when it is not the default main.nsl (so just so the url looks nicer) -->
@@ -33,13 +34,12 @@
                 src="scenarios/{scenario.dirName}/{scenario.metadata.icon}"
                 alt="{scenario.dirName}/{scenario.metadata.icon}"
             />
-            <img
+            <!-- <img
                 class="file-top"
                 src="/icons/file-top.svg"
                 alt="the top of a file"
-                height="200%"
-                width="100%"
-            />
+            /> -->
+            <div class="folder"></div>
         </a>
     {/each}
 </div>
@@ -50,13 +50,51 @@
     .file-top {
         position: absolute;
         left: 0px;
-        top: -80%;
-        width: 100%;
-    }
-
-    .card:hover .file-top {
-        transition: all 0.1s;
         top: 0%;
+        width: 100px;
+        height: 100%;
+    }
+    @keyframes flip {
+        0% {
+            transform: perspective(1000px) rotate3d(1, 0, 0, 0deg)
+                translateY(0px);
+        }
+        50% {
+            transform: perspective(1000px) rotate3d(1, 0, 0, -180deg);
+        }
+
+        100% {
+            transform: perspective(1000px) rotate3d(1, 0, 0, -300deg);
+            opacity: 0;
+        }
+    }
+    .card:hover .folder,
+    .card:focus-within .folder {
+        animation: flip 1s ease-out forwards;
+    }
+    .folder {
+        width: 105%;
+        height: 350px;
+        margin: 5em auto 2em;
+        border-radius: 25px 5px 25px 25px;
+        filter: drop-shadow(0 0 0.75rem grey);
+        background: rgb(202, 171, 58);
+        position: absolute;
+        top: 10px;
+        left: -10px;
+        transform-origin: bottom;
+    }
+    /* Source: https://stackoverflow.com/questions/71550110/is-it-possible-to-achieve-a-folder-shape-div-container-using-css */
+    .folder::before {
+        content: "";
+        position: absolute;
+        top: -18px;
+        right: 0;
+        width: 200px;
+        height: 25px;
+        background: rgb(202, 171, 58);
+        border-radius: 0 25px 0 0;
+        clip-path: path("M200 0H40c-25 2-15 16-40 18L200 100Z");
     }
 
     .card-container {
@@ -64,23 +102,24 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
+        justify-content: flex-start;
         min-height: 100vh;
         padding: 1rem 0;
+        padding-bottom: 400px; /* Extra padding to allow scrolling further */
         margin: 0;
-        width: 50vw;
+        width: 100vw;
         gap: 10px;
     }
 
     .card {
-        position: absolute;
-        top: 0;
+        position: sticky;
+        top: 200px;
         left: 0;
-        width: 100%;
+        width: 50%;
         min-height: 400px;
         background-color: #fff;
         border: 2px solid #ddd;
-        border-radius: 8px;
+        border-radius: 8px 8px 50px 50px;
         padding: 20px;
         text-decoration: none;
         color: inherit;
@@ -90,11 +129,12 @@
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         z-index: 1;
         margin-top: 10px;
+        scroll-snap-align: center;
+        height: 8vh;
     }
 
     .card:hover {
         transform: translateX(10px) translateY(-10px); /* Pull out effect */
-        z-index: 50;
         box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
     }
     .title {
@@ -126,6 +166,7 @@
     }
 
     .preview {
-        width: 50%;
+        max-height: 50%;
+        object-fit: cover;
     }
 </style>
