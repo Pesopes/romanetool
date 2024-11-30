c1af7a690cd613bbb73b670fd98074ecb62c14a0
Pesopes <jeniktelefon@gmail.com>
2024-11-30 11:46:38 +0100

    pause text also for ? and !

--- src/routes/game/[scenario]/dialogue.svelte
+++ src/routes/game/[scenario]/dialogue.svelte
@@ -44,7 +44,11 @@
         let specialDelay = 0;
 
         // Wait longer when unescaped "." or ","
-        if (word.match(/(?<!\\)\./g)) {
+        if (
+            word.match(/(?<!\\)\./g) ||
+            word.includes("?") ||
+            word.includes("!")
+        ) {
             specialDelay = 300;
         } else if (word.match(/(?<!\\),/g)) {
             specialDelay = 100;
